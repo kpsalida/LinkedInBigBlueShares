29054c58b92f9942d938f6ee4fb4037e72f6d3de
Katerina Psallida <katherinepsallida@gmail.com>
2024-05-26 16:32:38 +0300

    LAB1 Push

---
+++ Lab 1 Exploratory Data Analysis - Netflix/exploration/age_restrictions.py
@@ -0,0 +1,12 @@
+agr = {
+    "G":"General Audience",
+    "TV-G":"General Audience",
+    "PG" :"Parental Guidance", 
+    "PG-13":"Parental Guidance Strongly",
+    "TV-14" :"Parental Guidance Strongly",
+    "NC-17":"Adults Only",
+    "TV-MA" :"Adults Only",
+    "R":"Adults Only",
+    "TV-Y" :"Young Children",
+    "TV-Y7" : "Older Children",
+}
